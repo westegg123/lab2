1ce8807b67edc58370f67cbe34159c93e642a515
Andreas Komissopoulos <andreas1@magneton.cs.uchicago.edu>
2018-10-27 14:17:17 -0500

    idk

--- src/pipe.c
+++ src/pipe.c
@@ -4,7 +4,6 @@
  * ARM pipeline timing simulator
  */
 
-#include "sim.c"
 #include "pipe.h"
 #include "shell.h"
 #include "helper.h"
@@ -119,8 +118,7 @@ void pipe_stage_wb() {
 void pipe_stage_mem() {
 	clear_MEM_WB_REGS();
 	parsed_instruction_holder INSTRUCTION_HOLDER = get_holder(CURRENT_REGS.ID_EX.instruction);
-	CPU_State NEXT_STATE = CURRENT_STATE;
-	INSTRUCTION = CURRENT_REGS.ID_EX.instruction;
+	// CPU_State NEXT_STATE = CURRENT_STATE;
 	if (INSTRUCTION_HOLDER.format == 1) {
 		printf ("SOMETHING WEIRD HAPPENING - R INSTR SHOULDNT GO TO MEM\n");
 	} else if (INSTRUCTION_HOLDER.format == 2) {
@@ -142,34 +140,72 @@ void pipe_stage_mem() {
 	CURRENT_REGS.MEM_WB.ALU_result = CURRENT_REGS.EX_MEM.ALU_result;
 }
 
+void handle_add() {
+	CURRENT_REGS.EX_MEM.ALU_result = CURRENT_REGS.ID_EX.primary_data_holder + CURRENT_REGS.ID_EX.secondary_data_holder;
+}
+
+void handle_adds() {
+	handle_add();
+	CURRENT_STATE.FLAG_Z = (CURRENT_REGS.ID_EX.primary_data_holder + CURRENT_REGS.ID_EX.secondary_data_holder) == 0 ? 1 : 0;
+	CURRENT_STATE.FLAG_N = (CURRENT_REGS.ID_EX.primary_data_holder + CURRENT_REGS.ID_EX.secondary_data_holder) < 0 ? 1 : 0;
+}
+
+void handle_and() {
+	CURRENT_REGS.EX_MEM.ALU_result = CURRENT_REGS.ID_EX.primary_data_holder & CURRENT_REGS.ID_EX.secondary_data_holder;	
+}
+
+void handle_ands() {
+	handle_and();
+	CURRENT_STATE.FLAG_Z = (CURRENT_REGS.ID_EX.primary_data_holder & CURRENT_REGS.ID_EX.secondary_data_holder) == 0 ? 1 : 0;
+	CURRENT_STATE.FLAG_N = (CURRENT_REGS.ID_EX.primary_data_holder & CURRENT_REGS.ID_EX.secondary_data_holder) < 0 ? 1 : 0;
+}
+
+void handle_eor() {
+	CURRENT_REGS.EX_MEM.ALU_result = CURRENT_REGS.ID_EX.primary_data_holder ^ CURRENT_REGS.ID_EX.secondary_data_holder;	
+}
+
+void handle_orr() {
+	CURRENT_REGS.EX_MEM.ALU_result = CURRENT_REGS.ID_EX.primary_data_holder | CURRENT_REGS.ID_EX.secondary_data_holder;
+}
+// R INSTR EXECUTE STAGE
 void pipe_stage_execute() {
 	clear_EX_MEM_REGS();
-	INSTRUCTION = CURRENT_REGS.ID_EX.instruction;
-	parsed_instruction_holder HOLDER = get_holder(INSTRUCTION);
+	parsed_instruction_holder HOLDER = get_holder(CURRENT_REGS.ID_EX.instruction);
 	CPU_State NEXT_STATE = CURRENT_STATE;
-	if (HOLDER.format == 1 || HOLDER.format == 2) {
-		execute();
-		CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rd];
-	} else if (HOLDER.format == 3) {
-		if (HOLDER.op == 0x7C2 || HOLDER.op == 0x1C2 || HOLDER.op == 0x3C2) {
-			execute();
-			CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rt];
-		} else {
-			if (HOLDER.format == 0x7C0) {
-				CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rn] + HOLDER.DT_address;
-				CURRENT_REGS.EX_MEM.data_to_write = NEXT_STATE.REGS[HOLDER.Rt];
-			} else if (HOLDER.format == 0x1C0) {
-				CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rn] + HOLDER.DT_address;
-				CURRENT_REGS.EX_MEM.data_to_write = get_memory_segment(0,7, NEXT_STATE.REGS[HOLDER.Rt]);
-			} else if (HOLDER.format == 0x3C0) {
-				CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rn] + HOLDER.DT_address;
-				CURRENT_REGS.EX_MEM.data_to_write = get_memory_segment(0,15, NEXT_STATE.REGS[HOLDER.Rt]);
-			} else if (HOLDER.format == 0x5C0) {
-				CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rn] + HOLDER.DT_address;
-				CURRENT_REGS.EX_MEM.data_to_write = get_memory_segment(0,31, NEXT_STATE.REGS[HOLDER.Rt]);
-			}
+	if (HOLDER.format == 1) {
+		if (HOLDER.opcode == 0x458 || HOLDER.opcode == 0x459) {
+			handle_add();
+		} else if (HOLDER.opcode == 0x558 || HOLDER.opcode == 0x559) {
+			handle_adds();
+		} else if (HOLDER.opcode == 0x450) {
+			handle_and();
+		} else if (HOLDER.opcode == 0x750) {
+			handle_ands();
+		} else if (HOLDER.opcode == 0x650) {
+			handle_eor();
+		} else if (HOLDER.opcode == 0x550) {
+			handle_orr():
 		}
-	}
+	// } else if (HOLDER.format == 3) {
+	// 	if (HOLDER.op == 0x7C2 || HOLDER.op == 0x1C2 || HOLDER.op == 0x3C2) {
+	// 		execute();
+	// 		CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rt];
+	// 	} else {
+	// 		if (HOLDER.format == 0x7C0) {
+	// 			CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rn] + HOLDER.DT_address;
+	// 			CURRENT_REGS.EX_MEM.data_to_write = NEXT_STATE.REGS[HOLDER.Rt];
+	// 		} else if (HOLDER.format == 0x1C0) {
+	// 			CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rn] + HOLDER.DT_address;
+	// 			CURRENT_REGS.EX_MEM.data_to_write = get_memory_segment(0,7, NEXT_STATE.REGS[HOLDER.Rt]);
+	// 		} else if (HOLDER.format == 0x3C0) {
+	// 			CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rn] + HOLDER.DT_address;
+	// 			CURRENT_REGS.EX_MEM.data_to_write = get_memory_segment(0,15, NEXT_STATE.REGS[HOLDER.Rt]);
+	// 		} else if (HOLDER.format == 0x5C0) {
+	// 			CURRENT_REGS.EX_MEM.ALU_result = NEXT_STATE.REGS[HOLDER.Rn] + HOLDER.DT_address;
+	// 			CURRENT_REGS.EX_MEM.data_to_write = get_memory_segment(0,31, NEXT_STATE.REGS[HOLDER.Rt]);
+	// 		}
+	// 	}
+	// }
 	// } else if (HOLDER.format == 4) {
 	// 	;
 	// } else if (HOLDER.format == 5) {
@@ -177,37 +213,40 @@ void pipe_stage_execute() {
 	// } else if (HOLDER.format == 6) {
 	// 	;
 	// }
+	}
 }
 
 void pipe_stage_decode() {
 	clear_ID_EX_REGS();
 	parsed_instruction_holder INSTRUCTION_HOLDER = get_holder(CURRENT_REGS.IF_ID.instruction);
 	CURRENT_REGS.ID_EX.PC = CURRENT_REGS.IF_ID.PC;
+	CURRENT_REGS.ID_EX.instruction = CURRENT_REGS.IF_ID.instruction;
 
 	if (INSTRUCTION_HOLDER.format == 1) { // R
 		CURRENT_REGS.ID_EX.primary_data_holder = CURRENT_STATE.REGS[INSTRUCTION_HOLDER.Rn];
 		CURRENT_REGS.ID_EX.secondary_data_holder = CURRENT_STATE.REGS[INSTRUCTION_HOLDER.Rm];
 
-	} else if (INSTRUCTION_HOLDER.format == 2) { // I
-		CURRENT_REGS.ID_EX.primary_data_holder = CURRENT_STATE.REGS[INSTRUCTION_HOLDER.Rn];
-		CURRENT_REGS.ID_EX.immediate = INSTRUCTION_HOLDER.ALU_immediate;
+	// } else if (INSTRUCTION_HOLDER.format == 2) { // I
+	// 	CURRENT_REGS.ID_EX.primary_data_holder = CURRENT_STATE.REGS[INSTRUCTION_HOLDER.Rn];
+	// 	CURRENT_REGS.ID_EX.immediate = INSTRUCTION_HOLDER.ALU_immediate;
 
-	} else if (INSTRUCTION_HOLDER.format == 3) { // D
-		CURRENT_REGS.ID_EX.primary_data_holder = CURRENT_STATE.REGS[INSTRUCTION_HOLDER.Rn];
-		CURRENT_REGS.ID_EX.immediate = sign_extend(INSTRUCTION_HOLDER.DT_address, 26, 2);
+	// } else if (INSTRUCTION_HOLDER.format == 3) { // D
+	// 	CURRENT_REGS.ID_EX.primary_data_holder = CURRENT_STATE.REGS[INSTRUCTION_HOLDER.Rn];
+	// 	CURRENT_REGS.ID_EX.immediate = sign_extend(INSTRUCTION_HOLDER.DT_address, 26, 2);
 
-	} else if (INSTRUCTION_HOLDER.format == 4) { // B
-		CURRENT_REGS.ID_EX.immediate = INSTRUCTION_HOLDER.BR_address;
+	// } else if (INSTRUCTION_HOLDER.format == 4) { // B
+	// 	CURRENT_REGS.ID_EX.immediate = INSTRUCTION_HOLDER.BR_address;
 
-	} else if (INSTRUCTION_HOLDER.format == 5) { // CB
-		CURRENT_REGS.ID_EX.primary_data_holder = sign_extend(INSTRUCTION_HOLDER.COND_BR_address, 19, 2);
+	// } else if (INSTRUCTION_HOLDER.format == 5) { // CB
+	// 	CURRENT_REGS.ID_EX.primary_data_holder = sign_extend(INSTRUCTION_HOLDER.COND_BR_address, 19, 2);
 
-	} else if (INSTRUCTION_HOLDER.format == 6) { // IM/IW
-		CURRENT_REGS.ID_EX.immediate = INSTRUCTION_HOLDER.MOV_immediate;
+	// } else if (INSTRUCTION_HOLDER.format == 6) { // IM/IW
+	// 	CURRENT_REGS.ID_EX.immediate = INSTRUCTION_HOLDER.MOV_immediate;
+	// }
 	}
-	CURRENT_REGS.ID_EX.instruction = CURRENT_REGS.IF_ID.instruction;
 }
 
+// OK
 void pipe_stage_fetch() {
 	clear_IF_ID_REGS();
 	CURRENT_REGS.IF_ID.instruction = mem_read_32(CURRENT_STATE.PC);
